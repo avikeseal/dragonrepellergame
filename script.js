4b4144cd7a2f29a60546fa3cebd8340b6da59c01
avikeseal <avikeseal@gmail.com>
2024-07-19 11:07:23 -0400

    initialized buttons

--- script.js
+++ script.js
@@ -30,3 +30,8 @@ const goldText = document.querySelector('#goldText');
 const monsterStats = document.querySelector('#mosnterStats');
 const monsterNameText = document.querySelector('#monsterName');
 const monsterHealthText = document.querySelector('#monsterHealth');
+
+//initialize buttons:
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
