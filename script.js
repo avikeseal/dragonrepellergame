1396fc4344e6b73f983a4d76e405bbcf96377af1
avikeseal <avikeseal@gmail.com>
2024-07-19 10:49:35 -0400

    created all the variables for the game

---
+++ script.js
@@ -0,0 +1,21 @@
+//three ways to declare a variable:
+//const var let
+//using let recommended
+
+let xp = 0;
+let health = 100;
+let gold = 50;
+let currentWeapon = 0;
+let fighting;
+let monsterHealth;
+// let inventory = 'stick'; //inventory can't store multiple items 
+//we're going to change the values of inventory to be an array
+
+//an array of 3 strings:
+//let inventory = ['stick', 'dagger', 'sword']
+
+//for now:
+//if we want to add elements we have to start with an array
+let inventory = ['stick'];
+
+//if the values never going to change you start the value with const:
